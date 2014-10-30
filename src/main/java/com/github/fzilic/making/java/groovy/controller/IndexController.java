c0fe00700b9670549af7537dda19e42142ef92ef
Franjo Žilić <frenky666@gmail.com>
2014-10-30 10:38:37 +0100

    initial stuff

---
+++ src/main/java/com/github/fzilic/making/java/groovy/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.github.fzilic.making.java.groovy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String indexPage() {
+        return "index";
+    }
+}
